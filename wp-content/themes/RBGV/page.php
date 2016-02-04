012547fb3c9f7a5c65dfbb356005a252d7c814b3
Jeam Alves <jeanmalves@gmail.com>
2016-02-04 21:12:26 -0200

    simple page.

---
+++ wp-content/themes/RBGV/page.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+	<div class="w-section featured-section">
+    <div class="w-container area-pagina">
+      <div data-ix="scroll-reveal-part-1" class="titulo">
+      	<?php the_post(); ?>
+        <h1 class="titulo-sessao texto-centro"><?php the_title(); ?></h1>
+      </div>
+      <div id="simple-page" data-ix="scroll-reveal-part2" class="conteudo paragrafo">
+	    <?php  	
+	      	if ( has_post_thumbnail() ) {
+			    the_post_thumbnail( 'large' );
+			} 
+      	?>
+        <?php the_content(); ?>
+      </div>
+    </div>
+  </div>
+<?php get_footer(); ?>
